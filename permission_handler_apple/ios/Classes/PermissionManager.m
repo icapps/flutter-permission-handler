5065fe64060c8c3d34dfe66c9b5060bc4ea34276
Koen Van Looveren <vanlooverenkoen.dev@gmail.com>
2022-11-22 16:02:22 +0100

    Update PermissionManager.m

--- permission_handler_apple/ios/Classes/PermissionManager.m
+++ permission_handler_apple/ios/Classes/PermissionManager.m
@@ -98,11 +98,7 @@
         case PermissionGroupLocation:
         case PermissionGroupLocationAlways:
         case PermissionGroupLocationWhenInUse:
-            #if PERMISSION_LOCATION
             return [[LocationPermissionStrategy alloc] initWithLocationManager];
-            #else
-            return [LocationPermissionStrategy new];
-            #endif
         case PermissionGroupMediaLibrary:
             return [MediaLibraryPermissionStrategy new];
         case PermissionGroupMicrophone:
